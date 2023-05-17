c61c0b2a70797511ad07aad7d414f70a88c52cae
José Manuel Cota <jmcota@ontica.org>
2023-05-16 22:01:36 -0600

    Initial commit

---
+++ Core/Empiria.Artifacts.AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿/* Empiria OnePoint Artifacts ********************************************************************************
+*                                                                                                            *
+*  Module   : Sofware Configuration Management               Component : Backend Layer                       *
+*  Assembly : Empiria.OnePoint.Artifacts.dll                 Pattern   : Assembly Attributes File            *
+*  Type     : None                                           License   : Please read LICENSE.txt file        *
+*                                                                                                            *
+*  Summary  : Provides software configuration management services.                                           *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+/*************************************************************************************************************
+* Assembly configuration attributes.                                                                         *
+*************************************************************************************************************/
+[assembly: AssemblyTrademark("Empiria and Ontica are either trademarks of La Vía Óntica SC or Ontica LLC.")]
+[assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(true)]
+[assembly: InternalsVisibleTo("Empiria.Artifacts.Tests")]
---
+++ Core/README.md
@@ -0,0 +1,13 @@
+﻿# Empiria OnePoint Software Configuration Management
+
+Provides software configuration management services.
+
+It can be connected with project management or compliance systems.
+
+## License
+
+This system is distributed by the GNU AFFERO GENERAL PUBLIC LICENSE.
+
+## Copyright
+
+Copyright © 2023-2023. La Vía Óntica SC, Ontica LLC and colaborators.
